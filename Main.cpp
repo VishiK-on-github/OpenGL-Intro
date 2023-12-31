93d76e344dee80230d967463c86958b5eebc38e7
VishiK-on-github <vck281000@gmail.com>
2023-12-31 17:25:31 +0530

    Triangle creation done

--- Main.cpp
+++ Main.cpp
@@ -2,6 +2,22 @@
 #include<glad/glad.h>
 #include<GLFW/glfw3.h>
 
+// Vertex Shader source code
+const char* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+
+// Fragment Shader source code
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(0.8f, 0.3f, 0.02f, 1.0f);\n"
+"}\n\0";
+
 
 int main() {
 
@@ -39,19 +55,98 @@ int main() {
 	// to render in (bottom-left is 0,0)
 	glViewport(0, 0, 800, 800);
 
-	// setting the background color to navy blue, last bit set to 1 - opaque, 0 transparent
-	glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+	// creating a vertex shader
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
 
-	// clean the back buffer and assign the new color to it
-	glClear(GL_COLOR_BUFFER_BIT);
+	// GPU cant understand source code so we compile to machine code
+	glCompileShader(vertexShader);
 
-	// only pixels on front buffer are displayed hence swap is needed
-	glfwSwapBuffers(window);
+	// creating a fragment shader
+	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+
+	// GPU cant understand source code so we compile to machine code
+	glCompileShader(fragmentShader);
+
+	// to use shader we need to wrap into shader program
+	GLuint shaderProgram = glCreateProgram();
+
+	// attaching shader to program
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+
+	// linking the program
+	glLinkProgram(shaderProgram);
+
+	// delete shaders because they are already linked
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	// defining a list of vertices
+	GLfloat vertices[] =
+	{
+		-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower left corner
+		0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower right corner
+		0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f // Upper corner
+	};
+
+	/* 
+	CPU & GPU communication is slow hence we try to send data in 
+	big batches these batches are called buffers.
+	We create reference containers for Vertex Array Object and 
+	Vertex Buffer Object.
+	*/
+
+	GLuint VAO, VBO;
+
+	// generating VAO and VBO with one object each - why is this ?
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+
+	// making the VAO the current vertex array object by binding it
+	glBindVertexArray(VAO);
+
+	// Bind the VBO specifying it's a GL_ARRAY_BUFFER
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+	// Introduce the vertices into the VBO
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	// Configure the Vertex Attribute so that OpenGL knows how to read the VBO
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+
+	// Enable the Vertex Attribute so that OpenGL knows to use it
+	glEnableVertexAttribArray(0);
+
+	// Bind both the VBO and VAO to 0 so that we don't accidentally modify the VAO and VBO we created
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+	
 
 	// respond to events perfomed before window is closedj
 	while (!glfwWindowShouldClose(window)) {
+
+		// Specify the color of the background
+		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+		// Clean the back buffer and assign the new color to it
+		glClear(GL_COLOR_BUFFER_BIT);
+		// Tell OpenGL which Shader Program we want to use
+		glUseProgram(shaderProgram);
+		// Bind the VAO so OpenGL knows to use it. 
+		// Why is there a need to bind VAO again and again ?
+		glBindVertexArray(VAO);
+		// Draw the triangle using the GL_TRIANGLES primitive
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		// Swap the back buffer with the front buffer
+		glfwSwapBuffers(window);
+
 		glfwPollEvents();
 	}
+
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteProgram(shaderProgram);
 
 	// destroying the created window object
 	glfwDestroyWindow(window);
